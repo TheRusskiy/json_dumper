337f3b1e517971dbc4577fd04ff080bf2ee8cd36
therusskiy <dimanrussian@gmail.com>
2017-08-14 01:22:25 +0400

    fix for gon patch

--- lib/json_dumper/patches/gon.rb
+++ lib/json_dumper/patches/gon.rb
@@ -3,10 +3,10 @@ if defined?(Gon)
   class Gon
     class << self
       def set_variable(name, value)
-        if value.is_a?(JsonDumper::Delayed)
-          value = Class.new.extend(JsonDumper::Helper).dumper_fetch(value)
+        if value.is_a?(::JsonDumper::Delayed)
+          value = Class.new.extend(::JsonDumper::Helper).dumper_fetch(value)
         end
-        current_gon.gon[JsonDumper::KeyTransformer.camelize(name)] = value
+        current_gon.gon[::JsonDumper::KeyTransformer.camelize(name)] = value
       end
     end
   end

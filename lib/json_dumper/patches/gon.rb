d35357dce1d009f20ffbfa96d09dcfb76867a802
therusskiy <dimanrussian@gmail.com>
2017-08-21 00:49:14 +0400

    transform any structure passed to gon

--- lib/json_dumper/patches/gon.rb
+++ lib/json_dumper/patches/gon.rb
@@ -5,6 +5,8 @@ if defined?(Gon)
       def set_variable(name, value)
         if value.is_a?(::JsonDumper::Delayed)
           value = Class.new.extend(::JsonDumper::Helper).dumper_fetch(value)
+        elsif value.respond_to?(:each)
+          value = ::JsonDumper::KeyTransformer.keys_to_camelcase(value)
         end
         current_gon.gon[::JsonDumper::KeyTransformer.camelize(name)] = value
       end
